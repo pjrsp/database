9e28830316538ac70ac6ff802878dbc66f1a051d
pjrsp <59156916+pjrsp@users.noreply.github.com>
2020-01-16 12:16:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+// var path = require('path');
 
-const port = 3000;
+const port = 3002;
 
 var app = new express();
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
